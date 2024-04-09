67d1fb1dab7b89e2734ff8718a8777fc9521682c
Hunine <hqhung201@gmail.com>
2024-04-09 11:25:26 +0700

    Add manage page

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
+import About from '@/views/About.vue'
 import Home from '@/views/Home.vue'
+import Manage from '@/views/Manage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  },
+  {
+    path: '/manage',
+    component: Manage
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/About.vue')
-    }
-  ]
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
